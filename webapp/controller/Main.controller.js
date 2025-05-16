e80be3e5736ecad581e26df35c5a13e2cec3059b
Renaat Haleydt <p150480@ap.be>
2025-05-16 10:49:56 +0000

    Toevoegen van filter mogelijkheid op Genre in de booksTable

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 
     return Controller.extend("booksapplication.controller.Main", {
         onInit() {
-            
+
         },
         // Dit event wordt getriggert wanneer een gebruiker op een rij klikt in de tabel. Deze wordt aangestuurd vanuit de Main.view
         onListItemPress: function (oEvent) {
@@ -24,14 +24,52 @@ sap.ui.define([
             if (match) {
                 sTitel = decodeURIComponent(match[1]);
                 sAuteur = decodeURIComponent(match[2]);
-            }else{
+            } else {
                 // No match found
             }
 
             // We veranderen de layout in het layoutModel om zo de App.view te laten weten welke layout er getoond moet worden.
             this.getOwnerComponent().getModel("layoutModel").setProperty("/layout", Library.LayoutType.TwoColumnsMidExpanded)
             // We zeggen de applicatie om de Detail Route in de manifest.json te tonen.
-            this.getOwnerComponent().getRouter().navTo("Detail", {title: sTitel, author: sAuteur})
+            this.getOwnerComponent().getRouter().navTo("Detail", { title: sTitel, author: sAuteur })
         },
+        filterTablePressed(oEvent) {
+            let oTable = this.byId("booksTable"),
+                oFilterBar = this.getView().byId("filterbar"),
+                oBinding = oTable.getBinding("items")
+
+
+            let aTableFilters = oFilterBar.getFilterGroupItems().reduce(function (aResult, oFilterGroupItem) {
+                let oControl = oFilterGroupItem.getControl(),
+                    aFilters = [];
+
+                let aSelectedKeys = oControl.getSelectedKeys();
+                if (aSelectedKeys.length > 0) {
+                    let aSubFilters = aSelectedKeys.map(function (sSelectedKey) {
+                        return new sap.ui.model.Filter({
+                            path: oFilterGroupItem.getName(),
+                            operator: sap.ui.model.FilterOperator.EQ,
+                            value1: sSelectedKey
+                        });
+                    });
+
+                    // Voeg alle subfilters apart toe aan aFilters (flatten)
+                    aFilters.push(...aSubFilters);
+                }
+
+                if (aFilters.length > 0) {
+                    aResult.push(new sap.ui.model.Filter({
+                        filters: aFilters,
+                        and: false // Of true, afhankelijk van of je een OF of EN zoekt
+                    }));
+                }
+
+                return aResult;
+
+            }, []);
+
+
+            oBinding.filter(aTableFilters);
+        }
     });
 });
