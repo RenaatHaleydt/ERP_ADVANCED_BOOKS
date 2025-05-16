c736cb059b953a589fb83ed85f018f5b241599e1
Renaat Haleydt <p150480@ap.be>
2025-05-16 09:44:36 +0000

    Binding voor het aangeklikte boek toegevoegd in de Detail Page zodat deze informatie verschijnt op deze pagina

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -5,6 +5,31 @@ sap.ui.define([
 
     return Controller.extend("booksapplication.controller.Detail", {
         onInit() {
+            // Wanneer deze controller geopend wordt weten we dat de gebruiker een boek geslecteerd heeft uit de lijst in de Main Page.
+            // We nemen de router en hangen er een event aan vast.
+            // Wanneer de router het Detail pattern (uit de manifest) matched, dan mag er een methode in werking treden. 
+            // Dit gaat over de onBookPatternMatched methode
+            let oRouter = this.getOwnerComponent().getRouter()
+                oRouter.getRoute("Detail").attachPatternMatched(this._onBookPatternMatched, this);
+        },
+        _onBookPatternMatched(oEvent){
+            // In de manifest.json zie je dat we in de Detail Route (lijn 101) een variabelen {title} en {author} hebben toegevoegd.
+            // Deze wordt meegegeven door de Main controller wanneer de gebruiker op een boek heeft geklikt in de lijst.
+            // Deze verwijst naar de titel en auteur van het boek in de lijst die gebonden is met het default OData model.
+            // Bijvoobeeld: '/BookSet(Title='Zie Scherp Scherper', Author='Tim Dams')
+            // Hieronder in de code halen we deze variabelen op, die ook in de url zitten.
+            let sPreddedBookTitle = oEvent.getParameter("arguments").title || "",
+                sPreddedBookAuthor = oEvent.getParameter("arguments").author || ""
+
+            // Nu willen we dit boek eigenlijk binden aan onze Detail Page. Zodat we de informatie uit het model hier kunnen gebruiken.
+            this.getView().bindElement({
+                path: `/BookSet(Title='${encodeURIComponent(sPreddedBookTitle)}',Author='${encodeURIComponent(sPreddedBookAuthor)}')`
+                // Hier kan je eventuele zaken aan toevoegen
+                // ,parameters: {
+                //     $expand: "...",
+                //     $filter: "Genre eq 'Programmeren'"
+                // }
+            });
         }
     });
 });
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -12,11 +12,26 @@ sap.ui.define([
         onListItemPress: function (oEvent) {
             // Hier gaan we het path opvragen van welke rij er geklikt is.
             let sPressedBookPath = oEvent.getSource().getBindingContext().getPath()
+            // Een voorbeeld kan het volgende zijn: "/BookSet(Title='ATOMIC%20HABITS',Author='JOSKE')"
+
+            // Reguliere expressie om Title en Author eruit te halen
+            const regex = /Title='([^']*)',Author='([^']*)'/
+
+            const match = sPressedBookPath.match(regex)
+            let sTitel = "",
+                sAuteur = ""
+
+            if (match) {
+                sTitel = decodeURIComponent(match[1]);
+                sAuteur = decodeURIComponent(match[2]);
+            }else{
+                // No match found
+            }
 
             // We veranderen de layout in het layoutModel om zo de App.view te laten weten welke layout er getoond moet worden.
             this.getOwnerComponent().getModel("layoutModel").setProperty("/layout", Library.LayoutType.TwoColumnsMidExpanded)
             // We zeggen de applicatie om de Detail Route in de manifest.json te tonen.
-            this.getOwnerComponent().getRouter().navTo("Detail", {book: sPressedBookPath})
+            this.getOwnerComponent().getRouter().navTo("Detail", {title: sTitel, author: sAuteur})
         },
     });
 });
